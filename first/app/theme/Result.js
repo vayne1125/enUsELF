0ab113f783652eef0e88b8948a79c5f2114dbe0e
huahua1018 <shelly10182433@gmail.com>
2022-07-28 18:30:55 +0800

    result

---
+++ first/app/theme/Result.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {View, Text, StyleSheet,  Dimensions,FlatList,Image} from 'react-native';
+
+export default class Result extends Component {
+  render() {
+    return (
+      <div>Result</div>
+    )
+  }
+}
