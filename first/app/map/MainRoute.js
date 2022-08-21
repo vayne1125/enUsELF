812211d8f56d7eef7141778bbb8e2847febda583
vayne1125 <vayne20011125@gmail.com>
2022-08-22 05:05:50 +0800

    --補上節日&優化選擇

--- first/app/map/MainRoute.js
+++ first/app/map/MainRoute.js
@@ -1,6 +1,7 @@
 export const MAIN_ROUTE_DATA = [
     {
       //24.1365593,120.6835935
+      //23.8269823,120.737534
       id: 'm1',
       latitude: 24.1365593,
       longitude: 120.6835935,
--- first/app/map/MapHome.js
+++ first/app/map/MapHome.js
@@ -8,17 +8,18 @@ import {
   Image,
 } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
-import MapView, { PROVIDER_GOOGLE, Marker, Polyline, Callout } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import { mapStyle } from './mapStyle';
-import { HOL_DATA } from './HolData';  //shop景點的資料
 import { MAIN_ROUTE_DATA } from './MainRoute'; //主路線的資料
 import { ORI_DATA } from './OriData'; //空資料 -> 初始化
 import { END_DATA } from './EndData';
 import DetailForMap from '../detail/DetailForMap';
 import Back from './Back';
+import Refresh from './Refresh';
 import MapViewDirections from 'react-native-maps-directions';
 import Hotplace from './Hotplace'
 import Shopplace from './Shopplace'
+import Holplace from './Holplace'
 const Map = () => {
   const API_key = 'AIzaSyDHq53RuJ511QN4rLqFmwLWiXA1_-nR7vY'
   const [once, setOnce] = useState(true);
@@ -48,11 +49,6 @@ const Map = () => {
     setShopPress(!shopPress); //打開
   }
   const onPressHandlerForHoliday = () => {
-    if (holPress) {
-      setHolData(ORI_DATA);
-    } else {
-      setHolData(HOL_DATA);
-    }
     setHolPress(!holPress); //打開
   }
   //取得當前位置
@@ -73,8 +69,8 @@ const Map = () => {
     const rt = [];
     const sz = array.length;
     var a = 1;
-    if(sz>20)
-      a = sz/20;
+    if (sz > 20)
+      a = sz / 20;
     var cnt = 0;
     array.map((i) => {
       cnt += 1;
@@ -86,36 +82,44 @@ const Map = () => {
 
   const hotSet = new Set();
   const shopSet = new Set();
-  const totalData = [];
-  const cityTable = {'台北':0,'新北':1,'桃園':2,'新竹':3,'彰化':4,'嘉義':5,'苗栗':6,'雲林':7,'南投':8,'台中':9,'台南':10};
+  const holSet = new Set();
 
-  const getDis = (pos,place) => {
-    rt = (pos.lat - place.location.lat)*(pos.lat - place.location.lat) + (pos.lng - place.location.lng)*(pos.lng - place.location.lng);
+  const getDis = (pos, place) => {
+    rt = (pos.lat - place.location.lat) * (pos.lat - place.location.lat) + (pos.lng - place.location.lng) * (pos.lng - place.location.lng);
     return Math.sqrt(rt)
   }
 
   //算出附近的點
   const getPlace = (array) => {
     //1 -> 111km  0.1 -> 11km
-    console.log(Hotplace.length);
     array.map((pos) => {
-      for(i=0;i<Hotplace.length;i++){
-        if(hotSet.has(Hotplace[i].id))
-          continue; 
-        if(getDis(pos,Hotplace[i]) <= 0.3){
+      for (i = 0; i < Holplace.length; i++) {
+        if (holSet.has(Holplace[i].id))
+          continue;
+        if (getDis(pos, Holplace[i]) <= 0.3) {
+          holData.push(Holplace[i]);
+          holSet.add(Holplace[i].id);
+        }
+      }
+      for (i = 0; i < Hotplace.length; i++) {
+        if (hotSet.has(Hotplace[i].id) || holSet.has(Hotplace[i].id))
+          continue;
+        if (getDis(pos, Hotplace[i]) <= 0.3) {
           hotData.push(Hotplace[i]);
           hotSet.add(Hotplace[i].id);
-       }
+        }
       }
-      for(i=0;i<Shopplace.length;i++){
-        if(shopSet.has(Shopplace[i].id))
-          continue; 
-        if(getDis(pos,Shopplace[i]) <= 0.3){
+      for (i = 0; i < Shopplace.length; i++) {
+        if (shopSet.has(Shopplace[i].id) || hotSet.has(Shopplace[i].id) || holSet.has(Shopplace[i].id))
+          continue;
+        if (getDis(pos, Shopplace[i]) <= 0.3) {
           shopData.push(Shopplace[i]);
           shopSet.add(Shopplace[i].id);
-       }
+        }
       }
     })
+    console.log('ok')
+    console.log(holData.length);
   }
 
   const SetData = (array) => {
@@ -133,6 +137,10 @@ const Map = () => {
     }
     //setOnce(false);
   }, [])
+
+  const [myLatitudeDelta, setMyLatitudeDelta] = useState(1.2);
+  // var myLatitudeDelta = 3.8;
+
   return (
     <View style={styles.container}>
 
@@ -140,7 +148,7 @@ const Map = () => {
       <DetailForMap
         entry={modalEntry}//傳進去的資料參數
         modalVisible={modalVisible}//可不可見
-        onClose={() => { setModalVisible(false);}}//關閉函式
+        onClose={() => { setModalVisible(false); }}//關閉函式
       />
       {/*浮動視窗-------------------------------------------------------------------------------*/}
 
@@ -182,6 +190,10 @@ const Map = () => {
 
 
       <MapView //todo:初始化位置 
+        moveOnMarkerPress={false}
+        //rotateEnabled
+        //zoomEnabled
+        onRegionChangeComplete={(e) => { setMyLatitudeDelta(e.latitudeDelta); console.log(e.latitudeDelta) }}
         //customMapStyle={mapStyle}
         provider={PROVIDER_GOOGLE}
         style={styles.mapStyle}
@@ -190,8 +202,8 @@ const Map = () => {
           //23.8269823,120.737534
           latitude: 23.8269823,
           longitude: 120.737534,
-          latitudeDelta: 0.7, //數字越小 地圖道路越大
-          longitudeDelta: 0.7,
+          latitudeDelta: myLatitudeDelta, //數字越小 地圖道路越大
+          longitudeDelta: 0,
         }}
         mapType="standard"
       >
@@ -204,115 +216,138 @@ const Map = () => {
               longitude: marker.longitude,
             }}
           >
-            <Image source={require('../../assets/pin/green.png')}/>
+            <Image source={require('../../assets/pin/green.png')} />
           </Marker>
         ))}
         <MapViewDirections
           //optimizeWaypoints={true}
           //24.1365593,120.6835935
           origin={{ latitude: 24.1365593, longitude: 120.6835935 }} //to do抓當前位置
-          destination={{ latitude: 23.517405, longitude: 120.7914543, }} //to do抓最遠目的地
+          destination={{ latitude: 23.517405, longitude: 120.7914543 }} //to do抓最遠目的地
           apikey={API_key}
           strokeWidth={3}
           strokeColor="#5f695d"
           onReady={(result) => {
             console.log("pol ready");
             if (once) {
+              //console.log("nono");
               SetData(result.coordinates);
             }
             setOnce(false);
           }}
         />
 
-        {(hotPress ? hotData : ORI_DATA).map((marker,index) => (
-          <Marker
-            tracksViewChanges={false}
-            key={index}
-            coordinate={{latitude:marker.location.lat,longitude:marker.location.lng}}
-            pinColor='red'
-            //todo 簡介 按下變色
-            onPress={(e) => {
-              setModalVisible(!modalVisible);
-              setModalEntry({
-                id: marker.place_id,
-                name: marker.name,
-                info: marker.info,
-                address: marker.myAddr,
-                star: marker.rating,
-                info: marker.name,
-                time: marker.opening_hours.map((i)=>{
-                  return i + '\n';
-                }),
-                city: marker.city,
-                region: marker.reg,
-                source:{
-                  uri:
-                  `https://maps.googleapis.com/maps/api/place/photo?photoreference=${marker.photo.photo_reference}&sensor=false&maxheight=${marker.photo.height}&maxwidth=${marker.photo.width}&key=${API_key}`
-                }
-              });
-              //marker.pinColor = 'green';  todo:他不是即時更新
-              //console.log(marker.pinColor);
-            }}
-          >   
-          <View style={styles.markerCss}>
-              <Text style={styles.markerText}>{marker.name}</Text>
-              <Image style={styles.markerImg}source={require('../../assets/pin/red.png')}/>
-          </View> 
-              
-            </Marker>
-        ))}
-        {holData.map((marker) => (
-          <Marker
-            key={marker.id}
-            coordinate={{
-              latitude: marker.latitude,
-              longitude: marker.longitude,
-            }}
-            pinColor={marker.pinColor}
-            onPress={(e) => {
-              setModalVisible(!modalVisible);
-              setModalEntry({
-                id: marker.place_id,
-                name: marker.name,
-                address: marker.myAddr,
-                star: marker.rating,
-                info: marker.name,
-                time:marker.opening_hours
-              });
-            }}
-          ></Marker>
-        ))}
+        {(hotPress ? hotData : ORI_DATA).map((marker, index) => {
+          if (marker.del >= myLatitudeDelta) {
+            //console.log(myLatitudeDelta);
+            return (
+              <Marker
+                tracksViewChanges={false}
+                key={index}
+                coordinate={{ latitude: marker.location.lat, longitude: marker.location.lng }}
+                pinColor='red'
+                //todo 簡介 按下變色
+                onPress={(e) => {
+                  setModalVisible(!modalVisible);
+                  setModalEntry({
+                    id: marker.place_id,
+                    name: marker.name,
+                    info: marker.info,
+                    address: marker.myAddr,
+                    star: marker.rating,
+                    info: marker.name,
+                    time: marker.opening_hours.map((i) => {
+                      return i + '\n';
+                    }),
+                    city: marker.city,
+                    region: marker.reg,
+                    source: {
+                      uri:
+                        `https://maps.googleapis.com/maps/api/place/photo?photoreference=${marker.photo.photo_reference}&sensor=false&maxheight=${marker.photo.height}&maxwidth=${marker.photo.width}&key=${API_key}`
+                    }
+                  });
+                  //marker.pinColor = 'green';  todo:他不是即時更新 method redraw
+                  //console.log(marker.pinColor);
+                }}
+              >
+                <View style={styles.markerCss}>
+                  <Text style={styles.markerText}>{marker.name}</Text>
+                  <Image style={styles.markerImg} source={require('../../assets/pin/red.png')} />
+                </View>
+              </Marker>)
+          }
+        })}
 
-        {(shopPress ? shopData : ORI_DATA).map((marker) => (
+{(holPress ? holData : ORI_DATA).map((marker) => {
+          if (marker.del >= myLatitudeDelta) {
+            return(
           <Marker
             key={marker.id}
-            coordinate={{latitude:marker.location.lat,longitude:marker.location.lng}}
-            pinColor='blue'
-            title={marker.name}
+            coordinate={{ latitude: marker.location.lat, longitude: marker.location.lng }}
             onPress={(e) => {
               setModalVisible(!modalVisible);
               setModalEntry({
                 id: marker.place_id,
                 name: marker.name,
                 address: marker.myAddr,
-                star: marker.rating,
+                // star: marker.rating,
                 info: marker.name,
-                time: marker.opening_hours.map((i)=>{
+                time: marker.opening_hours.map((i) => {
                   return i + '\n';
                 }),
                 city: marker.city,
                 region: marker.reg,
-                source:{
-                  uri:
-                  `https://maps.googleapis.com/maps/api/place/photo?photoreference=${marker.photo.photo_reference}&sensor=false&maxheight=${marker.photo.height}&maxwidth=${marker.photo.width}&key=AIzaSyDnwR27cx8SVopcsqtZ0nYfGXM_2LQbgGY`
+                source: {
+                  uri: marker.photo
                 }
               });
             }}
           >
-            <Image source={require('../../assets/pin/blue.png')}/>
-            <Text style={styles.markerText}>{marker.name}</Text>
+            <View style={styles.markerCss}>
+              <Text style={styles.markerText}>{marker.name}</Text>
+              <Image style={styles.markerImg} source={require('../../assets/pin/yellow.png')} />
+            </View>
           </Marker>
-        ))}
+            )}
+          })}
+
+        {(shopPress ? shopData : ORI_DATA).map((marker) => {
+          if (marker.del >= myLatitudeDelta) {
+            return (
+              <Marker
+                key={marker.id}
+                coordinate={{ latitude: marker.location.lat, longitude: marker.location.lng }}
+                pinColor='blue'
+                title={marker.name}
+                onPress={(e) => {
+                  setModalVisible(!modalVisible);
+                  setModalEntry({
+                    id: marker.place_id,
+                    name: marker.name,
+                    address: marker.myAddr,
+                    star: marker.rating,
+                    info: marker.name,
+                    time: marker.opening_hours.map((i) => {
+                      return i + '\n';
+                    }),
+                    city: marker.city,
+                    region: marker.reg,
+                    source: {
+                      uri:
+                        `https://maps.googleapis.com/maps/api/place/photo?photoreference=${marker.photo.photo_reference}&sensor=false&maxheight=${marker.photo.height}&maxwidth=${marker.photo.width}&key=${API_key}`
+                    }
+                  });
+                }}
+              >
+                <View style={styles.markerCss}>
+                  <Text style={styles.markerText}>{marker.name}</Text>
+                  <Image style={styles.markerImg} source={require('../../assets/pin/blue.png')} />
+                </View>
+              </Marker>
+            )
+          }
+        }
+        )}
 
         {endData.map((marker) => (
           <Marker
@@ -332,6 +367,9 @@ const Map = () => {
       </MapView>
       <Callout>
         {/* <Back /> */}
+        {/* <Refresh onPress={() => {
+          console.log('refresh');
+        }}></Refresh> */}
       </Callout>
     </View>
   );
@@ -414,11 +452,11 @@ const styles = StyleSheet.create({
     fontSize: 17,
     color: '#5f695d',
   },
-  markerCss:{
+  markerCss: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  markerText:{
+  markerText: {
     color: '#5f695d',
     fontSize: 15,
     fontWeight: "bold",
