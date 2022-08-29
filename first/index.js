0d1642a1c560279f37996f116e25227a0d02e3b1
vayne1125 <vayne20011125@gmail.com>
2022-08-29 17:03:49 +0800

    --新增歷史資料介面

--- first/index.js
+++ first/index.js
@@ -8,5 +8,7 @@ import {name as appName} from './app.json';
 import MapHome from './app/map/MapHome'
 import ItineraryHome from './app/final/ItineraryHome'
 import Itmes from './app/list/Items'
+// import CollectHome from './app/personal/collect/CollectHome'
+import HistoryHome from './app/personal/history/HistoryHome'
 
 AppRegistry.registerComponent(appName, () => App);
